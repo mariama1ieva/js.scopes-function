b09035dbb6942c7281f0a693d463539637b07195
mariama1ieva <maryamfa@code.edu.az>
2024-02-03 08:10:36 +0400

    added files

---
+++ practicee/assets/js/script.js
@@ -0,0 +1,206 @@
+"use strict";
+
+
+// alert("Hello world");
+
+
+// let person = prompt("please enter your name");
+
+// if(person == ""){
+//   console.log("invalid");
+// }
+// else{
+//   console.log(person);
+// }
+
+
+
+// let elem = confirm("Press a button!");
+// console.log(elem);
+
+
+ 
+
+// let worker1 = {
+//   id:1,
+//   name:"Haci",
+//   surname:"Ahmadov",
+//   age:29
+// }
+
+// let worker2 = {
+//   id:2,
+//   name:"Mermer",
+//   surname:"Ismayilova",
+//   age:23
+// }
+
+// let worker3 = {
+//   id:3,
+//   name:"Ali",
+//   surname:"Aslanov",
+//   age:27
+// }
+
+// let worker4 = {
+//   id:4,
+//   name:"Aydan",
+//   surname:"Sabanova",
+//   age:28
+// }
+
+
+
+// // // // let result = worker1.name + " " + worker1.surname;
+// // // // console.log(result);
+
+
+
+// const workers = [worker1,worker2,worker3,worker4];
+
+// for (let i = 0; i < workers.length; i++) {
+  
+// let result = workers[i].name + " " + workers[i].surname;
+// console.log(result);
+
+// }
+
+
+// let sumAge = 0;
+// for (let i = 0; i < workers.length; i++){
+// sumAge += workers[i].age;
+// }
+// console.log(sumAge);
+
+
+
+// let averageAge = sumAge / workers.length;
+
+
+// if(averageAge > 50){
+//   console.log("Old");
+// }
+// else{
+//   console.log("Young");
+// }
+
+
+
+
+// document.querySelector("h1").innerText = workers[1].name;
+
+
+
+// for (const item of workers) {
+//   console.log(item.name);
+// }
+
+
+
+// const elems = [1,2,3,4,5];
+
+// for (const item of elems) {
+//   console.log(item);
+// }
+
+
+
+// const product = {
+//   name:"Samsung",
+//   price:500,
+//   model:"Galaxy"
+// }
+
+
+// for (const key in product) {
+//   console.log(key + " " + product[key]);
+// }
+
+
+// for (const worker of workers) {
+//   for (const key in worker) {
+//     if(key == "name"){
+//       let objectData = '${key} - ${worker[key]}';
+//       console.log(objectData);
+//     }
+//   }
+// }
+
+
+
+
+// function showText(){
+//   let isActive = true;
+
+//   if(isActive){
+//     return;
+//   }
+  
+//   console.log("tested");
+// }
+// showText();
+
+
+// function showName(text){
+// console.log(text);
+// }
+
+
+// showName("Semed");
+
+
+
+
+// function showFullData(name,surname,age){
+// const fullData = `${name} ${surname} ${age}`;
+// console.log(fullData);
+// }
+
+
+// showFullData("Haci","Ahmadov",29);
+
+
+
+
+// function num(num = 100){
+//   console.log(num + 200);
+// }
+
+// num(5);
+
+
+
+// function getStudentsByAge(workers){
+//   let count = 0;
+//   for (const worker of workers) {
+//     if(worker.age > 20)
+//     count++;
+//   }
+//  count **= 2;
+//  return count;
+// }
+
+// console.log(getStudentsByAge(workers));
+
+
+
+
+let status = true;
+
+if(status){
+  let elem = 100;
+  console.log("Yes");
+}
+
+
+
+
+
+function test(){
+  let surname = "test";
+}
+
+
+
+
+
---
+++ tasks/assets/js/script.js
@@ -0,0 +1,95 @@
+"use strict"
+
+
+// TASK1
+// function divided(number){
+//     if(number%3== 0 && number%7==0){
+//         console.log("is divided")
+//     }else{
+//         console.log("is not divided")
+//     }
+// }
+
+// divided(35);  
+
+// TASK2
+// function number(j) {
+//     let number = 1;
+//     if (j > 0) {
+//         for (let i = 1; i <= j; i++) {
+//             number *= i;
+//         }
+//     } else {
+//         console.log("number is negative")
+//     }
+//     console.log(number);
+
+// }
+// number(3);
+// TASK3
+// function numbers(numb){
+//   let multiple=1;
+//   let sum=0;
+//     for (let i = 0; i < numb.length; i++) {
+//             multiple = numb[i]**2;
+//             sum+=multiple;
+//     }
+//     console.log(sum);
+
+    
+// }
+// let numb = [1,2,3,4,5,6];
+// numbers(numb);
+
+// TASK4
+
+// function login(mail,password){
+//     if(mail=="cavid@code.edu.az" && password=="12345"){
+//         console.log("login is success")
+//     }
+//     else{
+//         console.log("mail or password is wrong")
+//     }
+// }
+// let mail ="cavid@code.edu.az";
+// let password = "12345"
+// login(mail,password);
+
+// TASK5
+
+// function oddNumbers(numb){
+//     let sum=0;
+//     for (let i = 0; i < numb.length; i++) {
+//         if(numb[i]%2!=0){
+//        sum+=numb[i];
+
+//         }
+//     }
+//     console.log(sum);
+// }
+// let numb = [1,2,3,4,5,6,7];
+// oddNumbers(numb);
+
+// TASK6
+// function countNumbers(numb){
+//         let count=0;
+//         for (let i = 0; i < numb.length; i++) {
+//             if(numb[i]%2==0){
+//             count++;    
+//             }
+//         }
+//         console.log(count);
+       
+//     }
+//     let numb = [1,2,3,4,5,6,7];
+//     countNumbers(numb);
+
+
+
+
+
+
+
+
+
+
